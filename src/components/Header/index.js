459952f7c9e09a4abad324ba6ac94a2d75765b0c
Daniel Demmel <dain@ustwo.com>
2016-11-18 15:25:45 +0000

    Make navigation rendered from settings data.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,55 +1,36 @@
-import React from "react"
+import React, { PropTypes } from "react"
 import { Link } from "react-router"
 
 import styles from "./index.css"
 
-const Header = () => (
+const Header = (props, { metadata: { settings } }) => (
   <header className={ styles.header }>
     <nav className={ styles.nav }>
-      <Link
-        className={ styles.link }
-        to="/"
-      >
-        { "Home" }
-      </Link>
-      <Link
-        className={ styles.link }
-        to="/games"
-      >
-        { "Games" }
-      </Link>
-      <Link
-        className={ styles.link }
-        to="/about"
-      >
-        { "About us" }
-      </Link>
-      <Link
-        className={ styles.link }
-        to="/jobs"
-      >
-        { "Jobs" }
-      </Link>
-      <Link
-        className={ styles.link }
-        to="/support"
-      >
-        { "Support" }
-      </Link>
-      <Link
-        className={ styles.link }
-        to="/contact"
-      >
-        { "Contact" }
-      </Link>
-      <a
-        className={ styles.link }
-        href="https://medium.com/@ustwogames"
-      >
-        { "Blog" }
-      </a>
+      {
+        settings.nav.map((navItem) => (
+          navItem.url.includes('http') ?
+          <a
+            key={ navItem.url }
+            className={ styles.link }
+            href={ navItem.url }
+          >
+            { navItem.title }
+          </a> :
+          <Link
+            key={ navItem.url }
+            className={ styles.link }
+            to={ navItem.url }
+          >
+            { navItem.title }
+          </Link>
+        ))
+      }
     </nav>
   </header>
 )
+
+Header.contextTypes = {
+  metadata: PropTypes.object.isRequired,
+}
 
 export default Header
