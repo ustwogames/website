12ed063847fb6245af1e0b90fba347a4bc9868da
Daniel Demmel <dain@ustwo.com>
2016-11-24 18:57:30 +0000

    Add Mailchimp signup form.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -15,6 +15,37 @@ const Footer = (props, { metadata: { settings } }) => (
         }
       </div>
     </nav>
+
+    <div id="mc_embed_signup">
+      <form action="//ustwogames.us14.list-manage.com/subscribe/post?u=4fc4bb980aaf05ff749e5211c&amp;id=fdceefe663" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
+        <div id="mc_embed_signup_scroll">
+          <h2>Subscribe to our mailing list</h2>
+          <div className="indicates-required"><span className="asterisk">*</span> indicates required</div>
+          <div className="mc-field-group">
+            <label htmlFor="mce-EMAIL">Email Address  <span className="asterisk">*</span>
+          </label>
+          <input type="email" name="EMAIL" className="required email" id="mce-EMAIL" />
+        </div>
+        <div className="mc-field-group">
+          <label htmlFor="mce-FNAME">First Name </label>
+          <input type="text" name="FNAME" className="" id="mce-FNAME" />
+        </div>
+        <div className="mc-field-group">
+          <label htmlFor="mce-LNAME">Last Name </label>
+          <input type="text" name="LNAME" className="" id="mce-LNAME" />
+        </div>
+        <div id="mce-responses" className="clear">
+          <div className="response" id="mce-error-response" style={{ display: "none" }}></div>
+          <div className="response" id="mce-success-response" style={{ display: "none" }}></div>
+        </div>
+        <div style={{ position: "absolute", left: "-5000px" }} aria-hidden="true"><input type="text" name="b_4fc4bb980aaf05ff749e5211c_fdceefe663" tabIndex="-1" /></div>
+        <div className="clear">
+          <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button" />
+        </div>
+      </div>
+    </form>
+    </div>
+
     <small className={ styles.notices } dangerouslySetInnerHTML={ {__html: settings.footer_notices} } />
   </footer>
 )
