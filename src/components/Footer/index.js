beb8bb5f2e6d7ea98080a8e6b7af28233ee9ef41
Alfred <alfred@ustwo.com>
2016-12-06 12:35:26 +0100

    email signup styling

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -23,11 +23,8 @@ const Footer = (props, { metadata: { settings } }) => (
       <form action="//ustwogames.us14.list-manage.com/subscribe/post?u=4fc4bb980aaf05ff749e5211c&amp;id=fdceefe663" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
         <div id="mc_embed_signup_scroll">
           <div className="mc-field-group">
-            <h4>Subscribe to our mailing list</h4>
-            <label htmlFor="mce-EMAIL">Email Address
-          </label>
-          <input type="email" name="EMAIL" className="required email" id="mce-EMAIL" />
-          <input role='button' type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button" />
+          <input type="email" name="EMAIL" placeholder="Email address" className="required email" id="mce-EMAIL" />
+          <input role='button' type="submit" value="Subscribe to our mailing list" name="subscribe" id="mc-embedded-subscribe" className="button" />
         </div>
         <div id="mce-responses" className="clear">
           <div className="response" id="mce-error-response" style={{ display: "none" }}></div>
