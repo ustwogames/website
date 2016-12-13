ac2a8c5bddb7fa4df4fec19e5f6504404632a41f
Daniel Demmel <dain@ustwo.com>
2016-12-13 11:54:51 +0000

    Make store badges smaller; tweak support page and general styling.

--- content/support.md
+++ content/support.md
@@ -5,6 +5,6 @@ layout: Support
 hero: /assets/studio-light.jpg
 ---
 
- 
+Click on the links below to open the FAQs or open a new support ticket if you can’t find an answer!
 
 <!-- FAQ will be rendered below here -->
--- src/layouts/Support/index.js
+++ src/layouts/Support/index.js
@@ -40,16 +40,13 @@ class Support extends Component {
             {"type": "text/javascript", "innerHTML": `window.zEmbed||function(e,t){var n,o,d,i,s,a=[],r=document.createElement("iframe");window.zEmbed=function(){a.push(arguments)},window.zE=window.zE||window.zEmbed,r.src="javascript:false",r.title="",r.role="presentation",(r.frameElement||r).style.cssText="display: none",d=document.getElementsByTagName("script"),d=d[d.length-1],d.parentNode.insertBefore(r,d),i=r.contentWindow,s=i.document;try{o=s}catch(e){n=document.domain,r.src='javascript:var d=document.open();d.domain="'+n+'";void(0);',o=s}o.open()._l=function(){var o=this.createElement("script");n&&(this.domain=n),o.id="js-iframe-async",o.src=e,this.t=+new Date,this.zendeskHost=t,this.zEQueue=a,this.body.appendChild(o)},o.write('<body onload="document._l();">'),o.close()}("https://assets.zendesk.com/embeddable_framework/main.js","ustwogames.zendesk.com");`},
           ] }
         />
-        <h2>FAQs</h2>
+        <h2 className={ styles.faqHeading }>FAQs</h2>
         {/* <FaqList faqs={ settings.faqs } /> */}
-        <ul className={ styles.faqLinkList }>
-          <li>
-            <Link to="http://blog.monumentvalleygame.com/faq/">Monument Valley FAQ</Link>
-          </li>
-          <li>
-            <Link to="http://www.landsendgame.com/customer-support/">Land’s End FAQ</Link>
-          </li>
-        </ul>
+        <p>
+          <Link to="http://blog.monumentvalleygame.com/faq/">Monument Valley FAQ</Link>
+          <br/>
+          <Link to="http://www.landsendgame.com/customer-support/">Land’s End FAQ</Link>
+        </p>
         <p>
           <Button big onClick={ () => { window.zE.activate({hideOnClose: true}); } }>Create support ticket</Button>
         </p>
