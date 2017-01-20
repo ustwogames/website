b310e379897131dda0581c0719373996b5251a43
Dev <dev@ustwo.com>
2017-01-20 13:44:02 +0000

    Footer bits

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -19,22 +19,6 @@ const Footer = (props, { metadata: { settings } }) => (
       </ul>
     </nav>
 
-    <div id="mc_embed_signup">
-      <form action="//ustwogames.us14.list-manage.com/subscribe/post?u=4fc4bb980aaf05ff749e5211c&amp;id=fdceefe663" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
-        <div id="mc_embed_signup_scroll">
-          <div className="mc-field-group">
-          <input type="email" name="EMAIL" placeholder="Email address" className="required email" id="mce-EMAIL" />
-          <input role='button' type="submit" value="Subscribe to our mailing list" name="subscribe" id="mc-embedded-subscribe" className="button" />
-        </div>
-        <div id="mce-responses" className="clear">
-          <div className="response" id="mce-error-response" style={{ display: "none" }}></div>
-          <div className="response" id="mce-success-response" style={{ display: "none" }}></div>
-        </div>
-        <div style={{ position: "absolute", left: "-5000px" }} aria-hidden="true"><input type="text" name="b_4fc4bb980aaf05ff749e5211c_fdceefe663" tabIndex="-1" /></div>
-      </div>
-    </form>
-    </div>
-
     <small className={ styles.notices } dangerouslySetInnerHTML={ {__html: settings.footer_notices} } />
 
     <small className={ styles.hosting }>
@@ -43,13 +27,13 @@ const Footer = (props, { metadata: { settings } }) => (
         <span>Github</span>
         <img src="/assets/github-logo.svg" alt="Github logo" title="Github logo" />
       </Link>
-      .
+      <br/>
       Built and hosted by
       <Link to="https://www.netlify.com">
         <span>Netlify</span>
         <img src="/assets/netlify-logo.svg" alt="Netlify logo" title="Netlify logo" />
       </Link>
-      .
+
     </small>
   </footer>
 )
---
+++ src/components/MailingList/index.js
@@ -0,0 +1,16 @@
+
+<div id="mc_embed_signup">
+  <form action="//ustwogames.us14.list-manage.com/subscribe/post?u=4fc4bb980aaf05ff749e5211c&amp;id=fdceefe663" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
+    <div id="mc_embed_signup_scroll">
+      <div className="mc-field-group">
+      <input type="email" name="EMAIL" placeholder="Email address" className="required email" id="mce-EMAIL" />
+      <input role='button' type="submit" value="Subscribe to our mailing list" name="subscribe" id="mc-embedded-subscribe" className="button" />
+    </div>
+    <div id="mce-responses" className="clear">
+      <div className="response" id="mce-error-response" style={{ display: "none" }}></div>
+      <div className="response" id="mce-success-response" style={{ display: "none" }}></div>
+    </div>
+    <div style={{ position: "absolute", left: "-5000px" }} aria-hidden="true"><input type="text" name="b_4fc4bb980aaf05ff749e5211c_fdceefe663" tabIndex="-1" /></div>
+  </div>
+</form>
+</div>
