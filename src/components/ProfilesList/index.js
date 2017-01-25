73f6354f65897dbbe7e242c621f9b7f1f87dd323
Dev <dev@ustwo.com>
2017-01-25 11:45:16 +0000

    styling changes to profile list

--- src/components/ProfilesList/index.js
+++ src/components/ProfilesList/index.js
@@ -1,18 +1,18 @@
 import React, { PropTypes } from "react"
-import { Link } from "phenomic"
+
 
 import styles from "./index.css"
 
 const ProfilesList = ({ profiles }) => {
   return (
-    <ul className={ styles.listContainer }>
+    <ul id="#profiles" className={ styles.listContainer }>
         {
           profiles.map((profile) => (
             <li key={ profile.__url } className={ styles.listItem }>
-              <Link to={ profile.__url }>
+                <a href="#profiles">
                 <img src={ profile.thumbnail } />
-                <h2 className={ styles.heading }>{ profile.nickName || profile.firstName }</h2>
-              </Link>
+                <h5 className={ styles.heading }>{ profile.nickName || profile.firstName }</h5>
+                </a>
             </li>
           ))
         }
