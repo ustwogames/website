75bb9ab568f7d2530c2ee9b16fe8efb498a6f2b7
Phil Linnell <phil@ustwo.com>
2017-02-08 16:36:37 +0000

    Add blurb and link to ustwo on homepage

--- src/layouts/Homepage/index.js
+++ src/layouts/Homepage/index.js
@@ -22,6 +22,10 @@ const Homepage = (props, { collection }) => {
     <Page { ...props }>
       <GamesPosters games={ featuredGames } mainTitles />
       <GamesPosters games={ restOfGames } />
+      <div className="homepage-additional">
+        <h2>We're proud to be a part of the <a href="http://www.ustwo.com">ustwo</a> family.</h2>
+        <p><a href="http://www.ustwo.com"><img src="/assets/ustwo-logo.svg" alt="ustwo logo" className="homepage-ustwo-logo" /></a></p>
+      </div>
     </Page>
   )
 }
